d4c4f1e9d9530445efd1b8190c5af575f69765df
luwenjin <luwenjin@gmail.com>
2012-07-24 12:30:12 +0800

    能够对ce tag为closed

--- static/js/coreexp-list.js
+++ static/js/coreexp-list.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     $('.answer').on('click', 'a', on_click_tag_link);
+    $('td.en').on('click', 'a', on_click_close_button);
 });
 
 function on_click_tag_link(e) {
@@ -8,7 +9,7 @@ function on_click_tag_link(e) {
     var tag = el.attr('data-tag');
     var id = el.attr('data-id')
 
-    var url = '/coreexp/option/tag'
+    var url = '/coreexp/option/tag';
     $.post(url, {'_id': id, 'option': option, 'tag': tag}, function(data){
         if (data.status == 'ok'){
             $('a', el.parent()).removeClass('tagged');
@@ -24,6 +25,42 @@ function on_click_tag_link(e) {
             alert(data.message);
         }
     });
+}
+
+function on_click_close_button(e) {
+    var el = $(e.currentTarget);
+    var id = el.attr('data-id');
+    var current_tag = el.attr('data-tag');
+
+    var url = '/coreexp/tag';
+    var params = {'_id': id, 'action': 'replace'};
+    if (current_tag != 'closed')
+    {
+        params['tag'] = 'closed';
+    }
+    else
+    {
+        params['tag'] = null;
+    }
+
+    el.attr('disabled', true);
+    $.post(url, params, function(data)
+    {
+        if (data.status == 'ok'){
+            el.attr('data-tag', params['tag']);
+            if (params['tag']){
+                el.text('已关闭')
+            } else {
+                el.text('开放中')
+            }
+
+        } else {
+            alert(data.message);
+        }
+        el.removeAttr('disabled')
+    });
+
+
 }
 
 
